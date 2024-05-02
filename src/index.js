bc2b81df59c023674e92d04136999bd98482ca64
Wendie <wendymathabela@gmail.com>
2024-05-02 11:42:46 +0200

    Added basic search engine

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFromElement = document.querySelector("#search-form");
+searchFromElement.addEventListener("submit", handleSearchSubmit);
